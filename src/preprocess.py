c6e640ac0bcf8636c45ce9bc70b975cb8c1b8a7b
eli kasav <elykassab24@gmail.com>
2025-06-22 16:46:20 +0300

    finish the train by years

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,6 +8,6 @@ def calculate_monthly_trains_execution(dataFrame):
     
     df['date'] = pd.to_datetime(df['shana'].astype(str) + '-' + df['hodesh'].astype(str), format='%Y-%m')
 
-    grouped = df.groupby('date')['achuz-bitsua'].mean().reset_index()
+    grouped = df.groupby('date')['ahuz_bitzua'].mean().reset_index()
 
     return grouped.set_index('date')    
